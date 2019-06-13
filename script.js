8f924a53d406540e3c21f2e526f9a6492ae99606
Yerin Cha <lenacha32@gmail.com>
2019-06-13 15:50:39 -0700

    Add k6 and new relic configuration and modify server side to test stress for the server

---
+++ script.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 300,
+  duration: '180s',
+  rps: 300,
+};
+
+export default function () {
+  const res = http.get(`http://localhost:1111/api/restaurants/${Math.ceil(Math.random() * 9999999) + 1}/info`);
+  check(res, {
+    'status was 200': r => r.status === 200,
+    'status was 404': r => r.status === 404,
+    'status was 500': r => r.status === 500,
+    'transaction time OK': r => r.timings.duration < 2000,
+  });
+  // sleep(1);
+}
+
+// k6 run script.js
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const app = express();
+const axios = require('axios');
+const request = require('request');
 const PORT = 1111;
 
 app.use(express.static(path.join(__dirname, '../public')))
@@ -9,19 +12,34 @@ app.get('/restaurants/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.get('/restaurants/:id/info', (req, res) => {
-  res.redirect(`http://127.0.0.1:3002/api/restaurants/${req.params.id}/info`);
+app.get('/api/restaurants/:id/info', (req, res) => {
+  // res.redirect(`http://127.0.0.1:3002/api/restaurants/${req.params.id}/info`);
+  // axios.get(`http://127.0.0.1:3002/api/restaurants/${req.params.id}/info`)
+  //   .then((response) => {
+  //     res.send(response.data).status(200);
+  //   })
+  //   .catch((error) => {
+  //     res.sendStatus(500);
+  //   });
+  request
+    .get(`http://127.0.0.1:3002/api/restaurants/${req.params.id}/info`)
+    .on('response', (response) => {
+      res.send(response).status(200);
+    })
+    .on('error', (err) => {
+      res.sendStatus(500);
+    })
 });
 
-app.get('/restaurants/:id/reviews', (req, res) => {
-  res.redirect(`http://127.0.0.1:3001/api/restaurants/${req.params.id}/reviews`);
-});
+// app.get('/restaurants/:id/reviews', (req, res) => {
+//   res.redirect(`http://127.0.0.1:3001/api/restaurants/${req.params.id}/reviews`);
+// });
 
-app.get('/restaurants/:id/photos', (req, res) => {
-  res.redirect(`http://127.0.0.1:3000/api/restaurants/${req.params.id}/photos`);
-});
+// app.get('/restaurants/:id/photos', (req, res) => {
+//   res.redirect(`http://127.0.0.1:3000/api/restaurants/${req.params.id}/photos`);
+// });
 
-app.get('/restaurants/:id/googlereviews', (req, res) => {
-  res.redirect(`http://127.0.0.1:3003/api/restaurants/${req.params.id}/googlereviews`);
-});
+// app.get('/restaurants/:id/googlereviews', (req, res) => {
+//   res.redirect(`http://127.0.0.1:3003/api/restaurants/${req.params.id}/googlereviews`);
+// });
 app.listen(PORT, () => console.log(`Proxy server listening on port ${PORT}!`));
